3f7087d3481c122b1dd50b40cc949e5c3770a258
Gonzalo <66648967+gonzaloparrilla@users.noreply.github.com>
2020-09-16 17:41:39 -0300

    Add files via upload

---
+++ CarritoDjango/app/app/database.py
@@ -0,0 +1,9 @@
+MYSQL = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django-shop',
+        'USER': 'root',
+        'PASSWORD': '',
+        'PORT': '3306'
+    }
+}
